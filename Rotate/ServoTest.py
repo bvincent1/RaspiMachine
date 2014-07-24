4ef7a3a5c3056ed1c9634c734f580c5007d5a906
bvincent1 <vincent@ualberta.ca>
2014-07-24 14:03:30 -0600

    changed to correct pin

--- Rotate/ServoTest.py
+++ Rotate/ServoTest.py
@@ -3,10 +3,10 @@ from RPIO import PWM
 servo = PWM.Servo()
 
 # Set servo on GPIO17 to 1200 milis (1.2ms)
-servo.set_servo(17, 1200)
+servo.set_servo(18, 1200)
 
 # Set servo on GPIO17 to 2000 mili s (2.0ms)
-servo.set_servo(17, 2000)
+servo.set_servo(18, 2000)
 
 # Clear servo on GPIO17
-servo.stop_servo(17)
+servo.stop_servo(18)
