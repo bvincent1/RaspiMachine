5ccd8c39b553f0b655bd47472ba813f0622725e3
Benjamin Vincent <benjc.vincent@gmail.com>
2014-07-24 22:27:17 +0000

    built temp WORKING code

--- Rotate/ControlServo.py
+++ Rotate/ControlServo.py
@@ -1,17 +1,53 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
 import time
+from sys import argv
 
 pin = 18
-refresh_period = 20/1000
+refresh_period = 1
 
-GPIO.setmode(GPIO.BCM)
+def resetPosition():
+	for i in range(1, 100):
+		gpio.output(pin, False)
+		time.sleep(1.5/1000)
+		gpio.output(pin, True)
+		time.sleep(refresh_period)
 
-GPIO.setup(pin, GPIO.OUT)
-GPIO.output(pin, True)
+def rotateClockwise():
+	for i in range(1, 100):
+		gpio.output(pin, False)
+		time.sleep(1/1000)
+		gpio.output(pin, True)
+		time.sleep(refresh_period)
 
-while True:
-    for i in range(1, 100):
-        GPIO.output(pin, False)
-        time.sleep(1.5/1000)
-        GPIO.output(pin, True)
-        time.sleep(refresh_period)
+
+def rotateCounterwise():
+        for i in range(1, 100):
+		gpio.output(pin, False)
+                time.sleep(2/1000)
+                gpio.output(pin, True)
+                time.sleep(refresh_period)
+
+
+if __name__ == "__main__":
+	gpio.setmode(gpio.BCM)
+	gpio.setup(pin, gpio.OUT)
+	gpio.setwarnings(False)
+	pwm = gpio.PWM(18, 100)
+	pwm.start(5)
+
+	if len(argv) > 1:
+		angle = float(argv[1])
+		target = angle / 10.0 + 2.5
+		
+		pwm.ChangeDutyCycle(target)
+	else:
+		while True:
+			i = 0
+			for i in range(5):
+				pwm.ChangeDutyCycle(2.5)
+				time.sleep(refresh_period)
+
+			i = 0
+			for i in range(5):
+				pwm.ChangeDutyCycle(20.5)
+				time.sleep(refresh_period)
