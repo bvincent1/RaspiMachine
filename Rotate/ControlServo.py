918089e8b561e431e761190cd6b9184b0cdceef9
bvincent1 <vincent@ualberta.ca>
2014-08-07 19:07:14 -0600

    changed PIN code

--- Rotate/ControlServo.py
+++ Rotate/ControlServo.py
@@ -3,14 +3,13 @@ import time
 from RPIO import PWM
 from sys import argv
 
-PIN = 18
 DELAY = 1
 SERVO = PWM.Servo()
 
 def getFreq(target):
 	return target * 10 + 500
 
-def setServoAngle(angle):
+def setServoAngle(angle, PIN = 18):
 	if angle > 180 or angle < 0:
 		raise Exception("Invalid angle. Has to be between 0 and 180 degrees")
 	target = getFreq(angle)
@@ -27,12 +26,12 @@ if __name__ == "__main__":
 	elif len(argv) > 2:
 		# set pin and servo to new position from command line
 		PIN = int(argv[2])		
-		setServoAngle(int(argv[1]))
+		setServoAngle(int(argv[1]), PIN)
 		
 	elif len(argv) > 3:
 		# set pin and servo to new position from command line
 		DELAY = int(argv[3])	
 		PIN = int(argv[2])		
-		setServoAngle(int(argv[1]))
+		setServoAngle(int(argv[1]), PIN)
 
 	SERVO.stop_servo(PIN)
