67142ebf28cdeda1b18ba21386f93fc529fc36f8
Benjamin Vincent <benjc.vincent@gmail.com>
2014-09-02 22:04:48 +0000

    added new changes

--- SwitchOff.sh
+++ SwitchOff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Switching off the light: $(date)" >> /tmp/RaspiMachine.log
-sudo python /home/pi/RaspiMachine/Rotate/ControlServo.py 90
+sudo python /home/pi/RaspiMachine/Rotate/ControlServo.py 70
